db3b65c48573e1de9e9e2e325e2d60c0661b4726
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-05-20 17:04:35 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,18 @@
-#include "lib.h"
-
-bool controllo(char a){
-    return ((a >= 'A' && a <= 'Z')  ||  (a >= 'a' && a <= 'z' ));
-}
-char conversione(char b) {
-    if (b >= 'a' && b <= 'z') {
-        return b - 32;
-    } else if (b >= 'A' && b <= 'Z') {
-        return b + 32;
+bool controllo(char b) {
+    if ((b >= 'a') and (b <= 'z')  or  ((b >= 'A') and (b <= 'Z'))) {
+        return true;
+    } else {
+        return false;
     }
+}
+char conversione(char b){
+
+    if ((b >= 'a') and (b <= 'z')){
+        b-=32;
+        return b;
+    }else if ((b >= 'A') and (b <= 'Z')){
+        b+=32;
+        return b;
+    }else
+        return false;
 }
