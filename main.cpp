8859df3b5b654d6ef9a696f8c8ca5f1921c3668c
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-05-20 15:01:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-  return 0;
+
+    char lettera;
+
+    cin >> lettera;
+
+    if(controllo(lettera)){
+        cout << true << endl;
+        cout << conversione(lettera) << endl;
+    }else
+        cout << false << endl;
+
+    return 0;
 }
